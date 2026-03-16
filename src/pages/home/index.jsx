c33622df1d8d4907570dd0a4636da572e3aac4b3
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-03-16 10:57:30 -0300

    aula 16-03

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,16 @@
+import styles from './index.module.css';
+
 function Home() {
     return (
-        <>
+        <div className={styles.container}>
             <h1>Aula React com VITE JS</h1>            
-        </>
+            <div className={styles.containerItens}>
+                <h2>Exemplos</h2>
+            </div>
+            <div className={styles.containerItens}>
+                <h2>Atividades</h2>
+            </div>
+        </div>
     )
 }
 
