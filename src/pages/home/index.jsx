44e0a280cb2ece3dde35f8d7540350588ff336b6
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-03-17 10:50:09 -0300

    aula 17-03

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router";
+
 import styles from './index.module.css';
 
 function Home() {
@@ -6,6 +8,10 @@ function Home() {
             <h1>Aula React com VITE JS</h1>            
             <div className={styles.containerItens}>
                 <h2>Exemplos</h2>
+
+                <Link to="/exemplo/1">Exemplo 1 - Componente básico</Link>
+                <Link to="/exemplo/2">Exemplo 2 - Uso de estilização com module</Link>
+
             </div>
             <div className={styles.containerItens}>
                 <h2>Atividades</h2>
