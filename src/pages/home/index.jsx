f8a65747aa44fcdcc59c34b0d325dc5097cb03a9
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-03-23 10:18:19 -0300

    Atividade 1

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -15,6 +15,8 @@ function Home() {
             </div>
             <div className={styles.containerItens}>
                 <h2>Atividades</h2>
+
+                <Link to="/atividade/1">Atividade 1 - Uso CSS</Link>
             </div>
         </div>
     )
