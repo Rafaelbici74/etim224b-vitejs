7e79897082741a4a4d67e972610d511a2a0d69a1
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-03-24 10:55:35 -0300

    aula 24-03

--- src/pages/atividades/atv-01/index.jsx
+++ src/pages/atividades/atv-01/index.jsx
@@ -1,9 +1,11 @@
+import styles from './index.module.css';
+
 function Atividade01() {
     return (
-        <>
-            <h1>Atividade 1</h1>
+        <div className={styles.container}>
+            <h1 className={styles.titulo}>Atividade 1</h1>
             <p>Componente básico para renderizar tela com React</p>
-        </>
+        </div>
     )
 }
 
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -11,12 +11,14 @@ function Home() {
 
                 <Link to="/exemplo/1">Exemplo 1 - Componente básico</Link>
                 <Link to="/exemplo/2">Exemplo 2 - Uso de estilização com module</Link>
+                <Link to="/exemplo/3">Exemplo 3 - State</Link>
 
             </div>
             <div className={styles.containerItens}>
                 <h2>Atividades</h2>
 
                 <Link to="/atividade/1">Atividade 1 - Uso CSS</Link>
+                <Link to="/atividade/2">Atividade 1 - Uso State</Link>
             </div>
         </div>
     )
