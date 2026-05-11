ff110c01e7a1ce7b1ac453b818b95ba2d616e1e4
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-05-11 15:27:28 -0300

    material slide 11-05 - tarde

--- src/pages/temp/index.jsx
+++ src/pages/temp/index.jsx
@@ -6,11 +6,37 @@
  */
 
 // npm i lucide-react
+
+import { useState, useEffect } from 'react';
 import { UserPlus, CheckCircle, Edit, ArrowRight, ArrowLeft, Plus, Trash2, Save, Shirt } from 'lucide-react';
 
 import styles from './index.module.css';
 
 export default function Exemplo06() {
+
+    const [categorias, setCategorias] = useState([]);
+
+    const [equipe, setEquipe] = useState({
+        nome: '',
+        corPrimaria: '#000000',
+        email: '',
+        categoria: '',
+    });
+
+    useEffect(() => {
+        setCategorias(["Futsal", "Volei", "Basquete"]);
+    }, []);
+
+    const handleEquipeChange = (e) => {
+        const { name, value } = e.target;
+        setEquipe(prev => ({ ...prev, [name]: value }));
+    };
+
+    const handleExibeValor = () => {
+        console.log('Dados registrados');
+        
+    }
+
     return (
         <div className={styles.container}>
             <div className={styles.card}>
@@ -21,29 +47,34 @@ export default function Exemplo06() {
 
                     <div className={styles.field}>
                         <label className={styles.label}>Nome da equipe</label>
-                        <input name="nome" className={styles.input} placeholder="Ex: Galáticos FC" />
+                        <input name="nome" className={styles.input} value={equipe.nome} onChange={handleEquipeChange} placeholder="Ex: Galáticos FC" />
                     </div>
 
                     <div className={styles.row}>
                         <div className={styles.field}>
                             <label className={styles.label}>Cor Principal</label>
-                            <input name="corPrimaria" type="color" className={styles.color} />
+                            <input name="corPrimaria" type="color" className={styles.color} value={equipe.corPrimaria} onChange={handleEquipeChange} />
                         </div>
 
                     </div>
 
                     <div className={styles.field}>
                         <label className={styles.label}>E-mail do responsável</label>
-                        <input name="email" type="email" className={styles.input} placeholder="email@exemplo.com" />
+                        <input name="email" type="email" className={styles.input} value={equipe.email} onChange={handleEquipeChange} placeholder="email@exemplo.com" />
                     </div>
 
                     <div className={styles.field}>
                         <label className={styles.label}>Categoria</label>
-                        <select name="categoria" className={styles.select} >
+                        <select name="categoria" className={styles.select} value={equipe.categoria} onChange={handleEquipeChange}>
                             <option value="">Selecione...</option>
-                            {/* {categorias.map(c => <option key={c} value={c}>{c}</option>)} */}
+                            {categorias.map(c => <option key={c} value={c}>{c}</option>)}
                         </select>
                     </div>
+
+                    <button onClick={() => handleExibeValor()} className={styles.btnNext}>
+                        Próximo: Integrantes <ArrowRight size={18} />
+                    </button>
+
                 </div>
             </div>
         </div>
